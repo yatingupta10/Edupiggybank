1a3b670a38ec6e9939e29bce45073e3fc4bffcd6
supernoobabhi <abhijeet_chauhan@yahoo.co.in>
2016-02-13 15:24:43 +0530

    Passbook synced with Backend

--- Edupiggybank/piggybank/views.py
+++ Edupiggybank/piggybank/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Passbook
 
 def index(request):
     return render(request, 'piggybank/index.html', {})
 def mypassbook(request):
-    return render(request, 'piggybank/mypassbook.html')
+    entries = Passbook.objects.all()
+    return render(request, 'piggybank/mypassbook.html',{'entries':entries})
 # Create your views here.
